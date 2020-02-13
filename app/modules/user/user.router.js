ccdbc26fe1e26677a79fc6268d3e8cd16cf7164b
Riya <riya.zavi@experionglobal.com>
2020-02-13 10:33:23 +0530

    login-added

--- app/modules/user/user.router.js
+++ app/modules/user/user.router.js
@@ -2,15 +2,18 @@ const router = require('express').Router()
 
 
 
-const addUserValidate = require('./add_User/add_User.validator')
-const addUser = require('./add_User/add_User.controller')
+const addUserValidate = require('./add_User/add_User.validator');
+const addUser = require('./add_User/add_User.controller');
+const loginUser = require('./login/login.controller');
+const loginUserValidate = require('./login/login.validator')
 
 
 module.exports = () => {
     router.route('/api/users')
         .post(addUserValidate, addUser)
-
-
+        .get(loginUserValidate, loginUser)
+    router.route('/api/login')
+        .get(loginUserValidate, loginUser)
 
 
     return router;
