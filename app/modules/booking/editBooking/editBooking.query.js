c49ada0e1f2dd2e03375ba38785a73ee5f1709c0
Riya <riya.zavi@experionglobal.com>
2020-02-14 16:11:03 +0530

    edit a booking changes added

--- app/modules/booking/editBooking/editBooking.query.js
+++ app/modules/booking/editBooking/editBooking.query.js
@@ -1,9 +1,9 @@
 const { user_tickets } = require('../../../../models/index');
 const editBookingQuery = async(req) => {
-
     return user_tickets.update({ quantity: req.body.quantity }, {
         where: {
-            ticket_id: req.body.ticket_id
+            ticket_id: req.body.ticket_id,
+            user_id: req.params.id
         }
     })
 
