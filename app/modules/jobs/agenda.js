e30df1530d4673fd2c3af7dc2da5489a15263e79
Riya <riya.zavi@experionglobal.com>
2020-03-20 11:05:54 +0530

    agenda changes added

--- app/modules/jobs/agenda.js
+++ app/modules/jobs/agenda.js
@@ -2,36 +2,25 @@
 const { tickets } = require('../../../../models/index');
 
 const Agenda = require('agenda')
-const moogoose = require('mongoose');
-// moogoose.connect('mongodb://localhost/TicketManagement')
-//     .then(() => { console.log("connected to mongodb") })
-//     .catch(err => {
-//         console.log("error in connecting to db")
-//     })
-
 const mongoConnectionString = 'mongodb://127.0.0.1/agenda';
-const agenda = new Agenda({ db: { address: mongoConnectionString } });
-
+agenda = new Agenda({ db: { address: mongoConnectionString } });
 agenda.define('findExpiredTickets', async job => {
-        expTickets = await tickets.findAll({
-            showTime: Sequelize.literal(`showTime + 10,800,000`)
-        }, {
-            where: {
-                showTime: {
-                    [Op.lt]: moment().format()
-                }
+    expTickets = await tickets.findAll({
+        showTime: Sequelize.literal('showTime + 10,800,000')
+    }, {
+        where: {
+            showTime: {
+                [Op.lt]: moment.utc().valueOf()
             }
-        })
-        return expTickets.update({
-            isActive: false
-
-        })
-
+        }
     })
-    // (async function() {
+    console.log(expTickets)
+    return expTickets.update({
+        isActive: false
+
+    });
+
+})
 agenda.start();
-
-agenda.every('1 minute', 'findExpiredTickets');
-
-//});
+agenda.every('3 minute', 'findExpiredTickets');
 module.exports = { agenda }
